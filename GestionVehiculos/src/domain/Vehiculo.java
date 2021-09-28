22e4ef5766f44424f4723ae503f0a912783c307d
yesenia <millan98yesenia@gmail.com>
2021-09-28 11:55:43 +0200

    Proyecto de Gestion de Vehiculos, con dos paquetes

---
+++ GestionVehiculos/src/domain/Vehiculo.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author milla
+ */
+public class Vehiculo {
+    private String matricula;
+    private String marca;
+    private String modelo; 
+    private String color;
+    private double tarifa;
+    private boolean disponible;
+    
+    //------------------------------------------------
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    //-------------------------------------
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    //-----------------------------------------
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", color=" + color + ", tarifa=" + tarifa + ", disponible=" + disponible + '}';
+    }
+    
+    
+}
